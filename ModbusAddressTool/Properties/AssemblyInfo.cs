88455c0d3d78cf1c312070ef10d3ecc7201bf4a7
MeiQianDaRen <144216372+MeiQianDaRen@users.noreply.github.com>
2026-08-22 09:10:22 +0800

    Create AssemblyInfo.cs

---
+++ ModbusAddressTool/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Modbus RS485 地址修改工具")]
+[assembly: AssemblyDescription("Modbus RTU RS485 设备地址修改工具")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ModbusAddressTool")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("E8BDF5D6-5A68-4D7E-A8B4-5F6F2A6D0B31")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
